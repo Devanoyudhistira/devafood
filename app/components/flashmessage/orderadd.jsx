79d0a9d670c388fdf3c239a26f626b0004cc2100
Devanoyudhistira <devanotira@gmail.com>
2026-04-14 21:42:12 +0700

    fix:flash message animation

--- app/components/flashmessage/orderadd.jsx
+++ app/components/flashmessage/orderadd.jsx
@@ -20,10 +20,10 @@ export default function Orderadd({ show, pending,message }) {
     }, [show])
     return (
         <AnimatePresence >
-            <motion.div exit={{ opacity: 0, scale: 0 }} initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} className="fixed top-14 left-[6vw] text-md bg-green-100 border-2 gap-3 border-green-300 w-85 h-14 flex items-center justify-center rounded-2xl" >                
+            {showmessage && <motion.div exit={{ opacity: 0, scale: 0 }} initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} className="fixed top-14 left-[6vw] text-md bg-green-100 border-2 gap-3 border-green-300 w-85 h-14 flex items-center justify-center rounded-2xl" >                
                     <CheckCircleFill size={20} color="green" />
                     <h1 className="text-green-400 font-bold" > {message} </h1>                         
-            </motion.div>
+            </motion.div>}
             {pending && !showmessage && <motion.div exit={{ opacity: 0, scale: 0 }} initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} className="fixed top-14 left-[6vw] text-md bg-green-100 border-2 gap-3 border-green-300 w-85 h-14 flex items-center justify-center rounded-2xl" >                
                     <ClipLoader size={25} />
                     <h1 className="text-xl font-extrabold" > loading </h1>                         
