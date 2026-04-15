7992bb4aa18478c5d8236d1a9018b8188d658255
Devanoyudhistira <devanotira@gmail.com>
2026-04-15 16:46:42 +0700

    fix:route guard on preview

---
+++ app/preview/layout.jsx
@@ -0,0 +1,19 @@
+import { createClient } from "../supabase/server";
+import supabase from "../supabase/supabase";
+import { redirect } from "next/navigation";
+
+export default async function layout({ children }) {
+    const supabaseserver = await createClient()
+    const { data } = await supabaseserver.auth.getUser()
+    const { data: session } = await supabaseserver.auth.getSession()
+    if (!session.session) {
+        redirect("/table")
+    }
+    const userid = data?.user.id
+    const { data: tabledata } = await supabase.from("meja").select("*").eq("uuid", userid).single()
+    return (
+        <>
+            {children}
+        </>
+    )
+}
